156216cfbf6cbd4f6b29d3fa7e892a2db58f8516
Losiak99 <emillos111122@gmail.com>
2025-12-02 22:53:31 +0100

    H2, Swagger, SecurityConfig (#1)

---
+++ motoparts-server/src/main/java/pl/motoparts/motoparts_server/MotopartsServerApplication.java
@@ -0,0 +1,13 @@
+package pl.motoparts.motoparts_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotopartsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotopartsServerApplication.class, args);
+	}
+
+}
---
+++ motoparts-server/src/test/java/pl/motoparts/motoparts_server/MotopartsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.motoparts.motoparts_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotopartsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
